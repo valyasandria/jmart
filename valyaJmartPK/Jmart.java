42861a5ec2d66c5f6424621afa74e1e4d45c92e3
valyasandria <valya.sandria@ui.ac.id>
2021-09-11 10:55:15 +0700

    persiapan

--- valyaJmartPK/Jmart.java
+++ valyaJmartPK/Jmart.java
@@ -2,27 +2,46 @@ package valyaJmartPK;
 
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+ public static void main(String[] args){
+     System.out.println ("Welcome to Jmart!");
+ }
+ public int getPromo(){
+     return 0;
+ }
+ public String getCustomer() {
+     return "oop";
+ }
+ public float getDiscountPercentage(int before, int after){
+     float potongan;
+     if (before < after) {
+         potongan = 0;
+     }
+     else {
+         potongan = before-after;
+     }
+     return potongan;
+ }
+ public int getDiscountedPrice (int price, float discountPercentage){
+     float discountedPrice;
+     if (discountPercentage>100.0f){
+         discountedPrice = 0;
+     }
+     else{
+         discountedPrice = price - discountPercentage;
+     }
+     return discountedPrice;
+ }
+ public int getOriginalPrice(int discountedPrice, float discountedPercentage){
+     int originalPrice;
+     
+ }
+ public float getCommissionMultiplier(){
+     
+ }
+ public int getAdjustedPrice (int g){
+     
+ }
+ public int getAdminFee(int a){
+     
+ }
 }
