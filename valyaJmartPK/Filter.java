79a48d74951556f9a61dd09510f364cbd4f33c86
valyasandria <valya.sandria@ui.ac.id>
2021-10-02 11:06:29 +0700

    CS - Modul 4 - Lab PK

--- valyaJmartPK/Filter.java
+++ valyaJmartPK/Filter.java
@@ -11,13 +11,23 @@ public class Filter {
             {
                 pricetags.add(obj) ;
             }
-            
+            else if (less && obj.getAdjustedPrice() > value)
+            {
+                pricetags.add(obj) ;
+            }
         }
         return priceTags;
     }
 
     public void filterProductRating(ArrayList<ProductRating> list, double value, boolean less)
     {
-
+        if(less && obj.getAdjustedPrice() < value)
+        {
+            clear(ProductRating.getAverage());
+        }
+        else if (less && obj.getAdjustedPrice() > value)
+        {
+            clear(ProductRating.getAverage());
+        }
     }
 }
