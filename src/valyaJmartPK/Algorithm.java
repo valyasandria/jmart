8b939c4bdc3c4dbe502837326392abd93ce74d0d
valyasandria <valya.sandria@ui.ac.id>
2021-10-11 20:50:53 +0700

    PT - Modul 5 - Lab PK

--- src/valyaJmartPK/Algorithm.java
+++ src/valyaJmartPK/Algorithm.java
@@ -1,9 +1,48 @@
 package valyaJmartPK;
 
-import java.util.Iterator;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Algorithm {
+
+    //method collect
+
+    public static <T> List<T> collect(T[] array, T value)
+    {
+        List<T> list = new ArrayList<>();
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        for (int i = 0; i < array.length ; i++){
+            list.add(array[i]);
+        }
+        return collect(it, value);
+
+    }
+
+    public static <T> List<T> collect(Iterable<T> iterable, T value)
+    {
+        return null;
+    }
+
+    public static <T> List<T> collect(Iterator<T> iterator, T value)
+    {
+        final Predicate <T> pred = value::equals;
+        return collect(iterator, (T) pred);
+    }
+
+    public <T> List<T> collect(T[] array, Predicate<T> pred)
+    {
+        return null;
+    }
+
+    public <T> List<T> collect(Iterable<T> iterable, Predicate<T> pred)
+    {
+        return null;
+    }
+
+    public <T> List<T> collect(Iterator<T> iterator, Predicate<T> pred)
+    {
+        return null;
+    }
 
     //method count
     public static <T> int count(T[] array, T value)
@@ -87,6 +126,7 @@ public class Algorithm {
         return count;
     }
 
+    //method exist
     public <T> boolean exist (T[] array, T value)
     {
         final Iterator<T> it = Arrays.stream(array).iterator();
@@ -128,13 +168,100 @@ public class Algorithm {
         return false;
     }
 
+    //method max
     public <T> T max (T first, T second)
+    {
+
+        return null;
+    }
+
+    public <T> T max (T[] array)
+    {
+        T max = null;
+        for(T item: array) {
+            if (max == null) {
+                max = item;
+            }
+        }
+        return max;
+    }
+
+    public <T> T max (Iterable <T> iterable)
     {
         return null;
     }
 
+    public <T> T max (Iterator <T> iterator)
+    {
+        return null;
+    }
+
+    public <T, comparator> T max (T first, T second, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+    public <T, comparator> T max (T[] array, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+
+    public <T, comparator> T max (Iterable<T> iterable, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+
+    public <T, comparator> T max (Iterator<T> iterator, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+
+    //method min
     public <T> T min (T first, T second)
     {
         return null;
     }
+
+    public <T> T min (T[] array)
+    {
+        T min = null;
+        for(T item: array) {
+            if (min == null) {
+                min = item;
+            }
+        }
+        return min;
+    }
+
+
+    public <T> T min (Iterable <T> iterable)
+    {
+        return null;
+    }
+
+    public <T> T min (Iterator <T> iterator)
+    {
+        return null;
+    }
+
+    public <T, comparator> T min (T first, T second, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+    public <T, comparator> T min (T[] array, Comparator<? super T> comparator)
+    {
+
+        return null;
+    }
+
+    public <T, comparator> T min (Iterable<T> iterable, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+
+    public <T, comparator> T min (Iterator<T> iterator, Comparator<? super T> comparator)
+    {
+        return null;
+    }
+
+
 }
